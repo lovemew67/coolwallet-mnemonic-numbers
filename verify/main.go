93376f3e8ef2d271716bd2d273600d1180fea297
lovemew67 <lovemew67@gmail.com>
2024-11-24 10:10:17 +0800

    task: add seed to number conversion

--- verify/main.go
+++ verify/main.go
@@ -63,4 +63,24 @@ func main() {
 		}
 	}
 	log.Println("🥳 all words are in mapping table")
+
+	file, err = os.Create("../seed-number-conversion.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	type revertedRecord struct {
+		Word   string `csv:"WORD"`
+		Number string `csv:"NUMBER"`
+	}
+	var revertedRecords = make([]*revertedRecord, 0)
+	for _, record := range records {
+		revertedRecords = append(revertedRecords, &revertedRecord{Word: record.Word, Number: record.Number})
+	}
+
+	err = gocsv.MarshalFile(&revertedRecords, file)
+	if err != nil {
+		panic(err)
+	}
 }
